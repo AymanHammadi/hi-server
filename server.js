a2d7e28ae03324274bb7c1e5a50ccd765b8c427e
Ayman <86633489+AymanHammadi@users.noreply.github.com>
2024-08-26 21:54:29 +0300

    Resolved merge conflicts

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const connectDB = require('./config/db');
 
@@ -22,29 +21,4 @@ app.listen(PORT, () => {
 
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from server!' });
-=======
-const express = require('express');
-const connectDB = require('./config/db');
-
-// Connect Database
-connectDB();
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Server is up and running');
-});
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-
-// just to test server with api call
-
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from server!' });
->>>>>>> 1b2e7404c34694fbed57888889aef8133412f350
 });

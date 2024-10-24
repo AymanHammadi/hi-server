caf26d5497f0b3e55d6d28478eb268ebf8483823
Ayman <86633489+AymanHammadi@users.noreply.github.com>
2024-10-25 01:29:38 +0300

    Refactor routes and package.json

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
-const User = require('../models/User');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
-
-/* 
-  @route    POST api/users
-  @desc     Register user
-  @access   Public
+/*
+  Path: POST /api/users/register
+  Desc: Register a user
+  Access: Public
 */
 
-
-
-router.post('/', [
-  check('name', 'Name is required').notEmpty(),
-  check('email', 'Please include a valid email').isEmail(),
-  check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })],
+router.post(
+  "/register",
+  [
+    check("name", "Name is required").notEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
   async (req, res) => {
     console.log(req.body);
 
@@ -33,14 +36,16 @@ router.post('/', [
       // See if user exists
       let user = await User.findOne({ email });
       if (user) {
-        return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
       }
 
       // Register user if not exists
       user = new User({
         name,
         email,
-        password
+        password,
       });
 
       // Encrypt password
@@ -51,31 +56,130 @@ router.post('/', [
       // payload is the data which we want to send in the token
       const payload = {
         user: {
-          id: user.id
-        }
+          id: user.id,
+        },
       };
 
       // Sign the token
       jwt.sign(
         payload,
-        config.get('jwtSecret'),
+        config.get("jwtSecret"),
         { expiresIn: 360000 },
         (err, token) => {
-
           if (err) {
             console.error(err.message);
-            return res.status(500).send('Server Error');
+            return res.status(500).send("Server Error");
           }
           res.json({ token });
         }
       );
-
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server Error');
+      res.status(500).send("Server Error");
     }
   }
 );
 
+/*
+  Path: POST /api/users/login
+  Desc: Register a user
+  Access: Public
+*/
+
+router.post(
+  "/login",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    console.log(req.body);
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // Check user user exists
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      // payload is the data which we want to send in the token
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      // Sign the token
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) {
+            console.error(err.message);
+            return res.status(500).send("Server Error");
+          }
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+/*
+  Path: GET /api/users
+  Desc: Takes a token and returns the user information
+  Access: Private
+*/
+
+const auth = (req, res, next) => {
+  const token = req.header("x-auth-token");
+
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    console.error(err.message);
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
 
 // Connect Database
 connectDB();
@@ -8,14 +8,12 @@ const app = express();
 app.use(express.json());
 
 // Define Routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/posts', require('./routes/posts'));
-app.use('/api/profile', require('./routes/profile'));
-app.use('/api/users', require('./routes/users'));
+app.use("/api/posts", require("./routes/posts"));
+app.use("/api/profile", require("./routes/profile"));
+app.use("/api/users", require("./routes/users"));
 
-
-app.get('/', (req, res) => {
-  res.send('Server is up and running');
+app.get("/", (req, res) => {
+  res.send("Server is up and running");
 });
 
 const PORT = process.env.PORT || 4000;
@@ -24,9 +22,8 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-
 // just to test server with api call
 
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from server!' });
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
 });
